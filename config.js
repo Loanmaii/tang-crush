e20fe3f5b4136f009b060c8e7d3ae7e754db1251
Loanmaii <86753136+Loanmaii@users.noreply.github.com>
2021-07-04 11:35:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'anh à!',
+    introTitle: 'babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say em ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'anh không yêu em??',
-    btnNo: 'đúng, em gái à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'yêu emmmmmmm',
+    btnNo: 'đúng, anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh<3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    mess: 'anh biết mà 🥰. Yeu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay đi chơi vui vẻ nhé hihi.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //sơnngu
